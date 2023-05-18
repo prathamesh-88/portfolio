537a70685e00291e506c92f30d33a25e3381d92e
Prathamesh <prathameshtamanekar@gmail.com>
2023-05-19 01:21:58 +0530

    Code cleanup

--- pages/articles.tsx
+++ pages/articles.tsx
@@ -1,10 +1,7 @@
 import Head from "next/head";
-import Link from "next/link";
-import Layout, { siteTitle } from "@/components/layouts/commonLayout";
+import Layout from "@/components/layouts/commonLayout";
 import { getSortedPostsData } from "../utility/posts";
 import Articles from "@/components/articles";
-import Date from "../components/date";
-import utilStyles from "@/styles/utils.module.css";
 import { Article } from "@/types";
 
 interface ArticlesProps {
@@ -24,7 +21,7 @@ export default function Home({ allArticlesData }: ArticlesProps) {
   return (
     <Layout pageType="articles">
       <Head>
-        <title>{siteTitle}</title>
+        <title>Articles</title>
       </Head>
       <Articles allArticlesData={allArticlesData} />
     </Layout>
